a42bf4a1b02344ef4ef8a5fdbe9ee1a163b7613d
Athul Bhandary <athulbhandary28@gmail.com>
2025-09-21 14:17:38 +0530

    final fix

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,11 @@
 import "~/styles/globals.css";
 
 import { type Metadata } from "next";
-// import { Geist } from "next/font/google";
-import { SessionProvider } from "next-auth/react";
+import { Geist } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
+
+import { SessionProvider } from "next-auth/react";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -12,16 +13,16 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-// const geist = Geist({
-//   subsets: ["latin"],
-//   variable: "--font-geist-sans",
-// });
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" >
+    <html lang="en" className={`${geist.variable}`}>
       <body>
         <TRPCReactProvider>
           <SessionProvider>{children}</SessionProvider>
